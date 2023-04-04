2f23af219586881e587ad420fafac3fd3e9733ec
Aaronias4 <111252509+Aaronias4@users.noreply.github.com>
2023-04-04 13:50:22 -0300

    dfgh

--- Milbury_Pelletier_FinalProject/Milbury_Pelletier_FinalProject/SportsCar.cs
+++ Milbury_Pelletier_FinalProject/Milbury_Pelletier_FinalProject/SportsCar.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace Milbury_Pelletier_FinalProject
 {
-    internal class SportsCar
+    public class SportsCar : Cars
     {
+        public int horsePower;
+
+        public int HorsePower { get; set; }
+
+        public override string ToString()
+        {
+            string sportsCar = base.ToString() + "Horse Power:  " + horsePower.ToString();
+            return sportsCar;
+        }
     }
 }
