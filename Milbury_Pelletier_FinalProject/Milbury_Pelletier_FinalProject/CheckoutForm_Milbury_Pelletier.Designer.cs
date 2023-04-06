932a4ef10393f63648ac527268a1f9a463066b45
JPellet123 <jwpell7802@gmail.com>
2023-04-05 21:01:13 -0300

    Adding sfd

--- Milbury_Pelletier_FinalProject/Milbury_Pelletier_FinalProject/CheckoutForm_Milbury_Pelletier.Designer.cs
+++ Milbury_Pelletier_FinalProject/Milbury_Pelletier_FinalProject/CheckoutForm_Milbury_Pelletier.Designer.cs
@@ -35,16 +35,17 @@
             this.btnRemoveItem = new System.Windows.Forms.Button();
             this.btnBack = new System.Windows.Forms.Button();
             this.lstCheckout = new System.Windows.Forms.ListBox();
+            this.sfdReceipt = new System.Windows.Forms.SaveFileDialog();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(768, 10);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(576, 8);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(89, 84);
+            this.pictureBox1.Size = new System.Drawing.Size(67, 68);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 7;
             this.pictureBox1.TabStop = false;
@@ -52,39 +53,36 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(329, 32);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(247, 26);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(155, 16);
+            this.label1.Size = new System.Drawing.Size(127, 13);
             this.label1.TabIndex = 9;
             this.label1.Text = "Item\'s to be Checked Out";
             // 
             // btnPrint
             // 
-            this.btnPrint.Location = new System.Drawing.Point(179, 416);
-            this.btnPrint.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnPrint.Location = new System.Drawing.Point(134, 338);
             this.btnPrint.Name = "btnPrint";
-            this.btnPrint.Size = new System.Drawing.Size(127, 49);
+            this.btnPrint.Size = new System.Drawing.Size(95, 40);
             this.btnPrint.TabIndex = 10;
             this.btnPrint.Text = "Print Receipt";
             this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
             // 
             // btnRemoveItem
             // 
-            this.btnRemoveItem.Location = new System.Drawing.Point(513, 416);
-            this.btnRemoveItem.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnRemoveItem.Location = new System.Drawing.Point(385, 338);
             this.btnRemoveItem.Name = "btnRemoveItem";
-            this.btnRemoveItem.Size = new System.Drawing.Size(125, 49);
+            this.btnRemoveItem.Size = new System.Drawing.Size(94, 40);
             this.btnRemoveItem.TabIndex = 11;
             this.btnRemoveItem.Text = "Remove Items";
             this.btnRemoveItem.UseVisualStyleBackColor = true;
             // 
             // btnBack
             // 
-            this.btnBack.Location = new System.Drawing.Point(349, 416);
-            this.btnBack.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnBack.Location = new System.Drawing.Point(262, 338);
             this.btnBack.Name = "btnBack";
-            this.btnBack.Size = new System.Drawing.Size(127, 49);
+            this.btnBack.Size = new System.Drawing.Size(95, 40);
             this.btnBack.TabIndex = 12;
             this.btnBack.Text = "Back";
             this.btnBack.UseVisualStyleBackColor = true;
@@ -92,25 +90,30 @@
             // lstCheckout
             // 
             this.lstCheckout.FormattingEnabled = true;
-            this.lstCheckout.ItemHeight = 16;
-            this.lstCheckout.Location = new System.Drawing.Point(105, 65);
+            this.lstCheckout.Location = new System.Drawing.Point(79, 53);
+            this.lstCheckout.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.lstCheckout.Name = "lstCheckout";
-            this.lstCheckout.Size = new System.Drawing.Size(632, 324);
+            this.lstCheckout.Size = new System.Drawing.Size(475, 264);
             this.lstCheckout.TabIndex = 13;
+            // 
+            // sfdReceipt
+            // 
+            this.sfdReceipt.DefaultExt = "txt";
+            this.sfdReceipt.FileName = "Receipt";
             // 
             // CheckoutForm_Milbury_Pelletier
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(868, 554);
+            this.ClientSize = new System.Drawing.Size(651, 450);
             this.Controls.Add(this.lstCheckout);
             this.Controls.Add(this.btnBack);
             this.Controls.Add(this.btnRemoveItem);
             this.Controls.Add(this.btnPrint);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "CheckoutForm_Milbury_Pelletier";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "CheckoutForm_Milbury_Pelletier";
@@ -129,5 +132,6 @@
         private System.Windows.Forms.Button btnRemoveItem;
         private System.Windows.Forms.Button btnBack;
         private System.Windows.Forms.ListBox lstCheckout;
+        private System.Windows.Forms.SaveFileDialog sfdReceipt;
     }
 }
--- Milbury_Pelletier_FinalProject/Milbury_Pelletier_FinalProject/CheckoutForm_Milbury_Pelletier.cs
+++ Milbury_Pelletier_FinalProject/Milbury_Pelletier_FinalProject/CheckoutForm_Milbury_Pelletier.cs
@@ -4,7 +4,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Drawing.Text;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -25,6 +27,43 @@ namespace Milbury_Pelletier_FinalProject
             {
                 lstCheckout.Items.Add(cars[i]);
             }
+        }
+        public string path = "C:\\files";
+        private void CheckForSave()
+        {
+            
+            //Checks if the user wants to save the file, then either saves or does nothing.
+            //Sends nothing, returns nothing
+            DialogResult userSave = MessageBox.Show("Save Results to File?",
+                "Save File?", MessageBoxButtons.YesNo);
+            if (userSave == DialogResult.Yes)
+            {
+                DialogResult saveResult = sfdReceipt.ShowDialog();
+
+                if (saveResult != DialogResult.Cancel)
+                {
+                    StreamWriter saveStream = new StreamWriter(sfdReceipt.FileName);
+
+                    for (int i = 0; i <lstCheckout.Items.Count; i++)
+                    {
+                        
+                        saveStream.WriteLine(sfdReceipt);
+                    }
+                    saveStream.Close();
+                    this.Close();
+                    MessageBox.Show("File saved successfully to " + path);
+                }
+                else
+                {
+                    MessageBox.Show("File saved unsuccessfully to " + path);
+                    this.Close();
+                }
+            }
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            CheckForSave();
         }
     }
 }
