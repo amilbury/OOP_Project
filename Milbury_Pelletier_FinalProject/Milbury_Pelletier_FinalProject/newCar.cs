86750c9b07d9f20ed1f462190ba5fa9fa6a115b6
JPellet123 <jwpell7802@gmail.com>
2023-04-05 21:08:08 -0300

    adding newcar form

---
+++ Milbury_Pelletier_FinalProject/Milbury_Pelletier_FinalProject/newCar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Milbury_Pelletier_FinalProject
+{
+    public partial class newCar_Milbury_Pelletier : Form
+    {
+        public newCar_Milbury_Pelletier()
+        {
+            InitializeComponent();
+        }
+    }
+}
